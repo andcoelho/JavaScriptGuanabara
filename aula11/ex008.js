114d118e0d1ea4eba6415797049dc44663cce3aa
Anderson <andcoelho97@gmail.com>
2021-11-08 17:24:32 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+/*Testando node js - F8
+console.log('O console funcionou corretamente') */
+
+var vel = 60.5;
+
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+
+if(vel > 60){
+    console.log("Você ultrapassou a velocidade permitida, multado");
+
+}
+
+console.log("Diriga sempre usando o cinto de segurança!");
